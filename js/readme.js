30278b9e43a5e2308070d8f9744b35a1aec37164
KeaNy Chu <keanyc@gmail.com>
2014-10-24 15:15:36 +0800

    Updated

--- js/readme.js
+++ js/readme.js
@@ -3,29 +3,10 @@ function create_readme(basepath) {
 
     document.write(
         '<h1>Kearena</h1>' +
-        '<a href="#en" style="float:right; margin-top:-40px;">English</a><br>' +
-
-        '遊戲開發中，歡迎提供回饋和想法!<br>' +
-
-        '<h3>說明</h3>' +
-        '<b>按鍵 / 法術</b><br>' +
-        '1 - 暗言術: 痛<br>' +
-        '2 - 心靈震爆<br>' +
-        '3 - 恢復<br>' +
-        '4 - 快速治療<br>' +
-        '* 支援手機或滑鼠點選法術圖示。<br>' +
-        '<br>' +
-        '<b>法術</b><br>' +
-        '<img src="img/spell_shadow_shadowwordpain.jpg"> 暗言術: 痛<br>法力值8，7秒內每秒傷害3點生命值。<br>' +
-        '<img src="img/spell_shadow_unholyfrenzy.jpg"> 心靈震爆<br>法力值10，直接傷害18點生命值，冷卻時間3秒。<br>' +
-        '<img src="img/spell_holy_renew.jpg"> 恢復<br>法力值7，6秒內每秒治療2點生命值。<br>' +
-        '<img src="img/spell_holy_flashheal.jpg"> 快速治療<br>法力值9，治療12點生命值。<br>' +
-        '<br>' +
-        '<b>法力恢復</b><br>' +
-        '每5秒鐘恢復10點法力。<br>' +
-        '<br><a name="en"></a>' +
-
-        '<hr style="border-bottom:1px solid #333">This game still under development. Feel free to give feedback and idea!<br>' +
+        
+        '<hr style="border-bottom:1px solid #333">' +
+        '<p>A web game inspired by World of Warcraft. I\'m addict to the arena and love to play shadow priest. I would like to enjoy sometime when the server down or on my mobile phone. So I develop this game. Enjoy it.</p>' +
+        '<p>Feel free to send me feedback or fork this. You can leave message on issues or contact me via my email.</p>' +
 
         '<h3>Readme</h3>' +
         '<b>Key / Spell</b><br>' +
@@ -33,8 +14,12 @@ function create_readme(basepath) {
         '2 - Mind Blast<br>' +
         '3 - Renew<br>' +
         '4 - Flash Heal<br>' +
-        '* support mobile or mouse for clicking on spell icons.<br>' +
+        'Enter - Flash Heal<br>' +
+        'L - Log<br>' +
+        'R - Reload<br>' +
+        '/ or ? - Help<br>' +
         '<br>' +
+        
         '<b>Spell</b><br>' +
         '<img src="img/spell_shadow_shadowwordpain.jpg"> Shadow Word: 8 mana, Pain<br>damage 3 health points per second, tick 7 times.<br>' +
         '<img src="img/spell_shadow_unholyfrenzy.jpg"> Mind Blast<br>10 mana, direct damage 18 health points. Colddown 2 sec.<br>' +
@@ -44,6 +29,6 @@ function create_readme(basepath) {
         '<b>Mana regeneration</b><br>' +
         'Recovery 10 mana points every 5 sec.<br>' +
 
-        '<hr style="border-bottom:1px solid #333">2013 KeaNy Chu <a href="http://keanychu.info">http://keanychu.info</a><br>');
+        '<hr style="border-bottom:1px solid #333">2014 KeaNy Chu <a href="http://keanychu.com">http://keanychu.com</a><br>');
 
 }
